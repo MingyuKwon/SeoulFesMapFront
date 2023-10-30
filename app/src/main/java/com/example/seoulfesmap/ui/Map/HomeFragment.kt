300d131cb2a2058c2ded910955c8092ae9503d6a
Kyprosa <propolis200@gmail.com>
2023-10-30 16:24:08 +0900

    gps 되나요?

--- app/src/main/java/com/example/seoulfesmap/ui/Map/HomeFragment.kt
+++ app/src/main/java/com/example/seoulfesmap/ui/Map/HomeFragment.kt
@@ -2,7 +2,9 @@ package com.example.seoulfesmap.ui.Map
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,9 +13,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
@@ -37,6 +37,7 @@ class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+        requestLocationPermission()
     }
 
     @SuppressLint("MissingPermission")
@@ -44,18 +45,35 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
+        val locationManager: LocationManager by lazy {
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
+
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val mapView: FragmentContainerView = binding.mapFragment
-        requestLocationPermission()
         val mapFragment = childFragmentManager.findFragmentById(mapView.id) as MapFragment?
         mapFragment?.getMapAsync { mapView ->
             val cameraPosition = CameraPosition(LatLng(37.540693, 127.07023), 10.0)
             mapView.cameraPosition = cameraPosition
+
+            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (location != null) {
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                val cameraPosition = CameraPosition(currentLatLng, 10.0)
+                mapView.cameraPosition = cameraPosition
+
+                // 현재 위치를 네이버 지도에 표시
+                val currentLocationMarker = Marker()
+                currentLocationMarker.icon = OverlayImage.fromResource(R.drawable.current_location_icon)
+                currentLocationMarker.position = currentLatLng
+                currentLocationMarker.map = mapView
+            }
 
             val marker = Marker()
             marker.icon = OverlayImage.fromResource(R.drawable.icon)
@@ -93,45 +111,18 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // 권한이 사용자에 의해 허용됨
-                // 현재 위치를 가져오는 코드를 이곳에 추가할 수 있음
-            } else {
-                // 권한이 거부됨
-                Toast.makeText(requireContext(), "위치 권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
     private fun requestLocationPermission() {
-        val context: android.content.Context = requireContext()
-        val fineLocationPermission = Manifest.permission.ACCESS_FINE_LOCATION
-        val coarseLocationPermission = Manifest.permission.ACCESS_COARSE_LOCATION
-
-        if (ContextCompat.checkSelfPermission(
-                context,
-                fineLocationPermission
-            ) == PackageManager.PERMISSION_GRANTED
-            && ContextCompat.checkSelfPermission(
-                context,
-                coarseLocationPermission
-            ) == PackageManager.PERMISSION_GRANTED
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // 권한이 이미 허용되어 있음
-            // 현재 위치를 가져오는 코드를 이곳에 추가할 수 있음
-        } else {
-            // 권한을 요청
             ActivityCompat.requestPermissions(
                 requireActivity(),
-                arrayOf(fineLocationPermission, coarseLocationPermission),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 LOCATION_PERMISSION_REQUEST_CODE
             )
         }
     }
+
 }
