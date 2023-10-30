98ec43299ab3aefd525a62e4a08e3578e9612be9
Kyprosa <propolis200@gmail.com>
2023-10-30 17:00:19 +0900

    gps 권한 요청창 안뜸

--- app/src/main/java/com/example/seoulfesmap/ui/Map/HomeFragment.kt
+++ app/src/main/java/com/example/seoulfesmap/ui/Map/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
@@ -13,12 +14,16 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
 import com.example.seoulfesmap.R
 import com.example.seoulfesmap.databinding.FragmentHomeBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.MapFragment
@@ -53,12 +58,13 @@ class HomeFragment : Fragment() {
             requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
         }
 
-
+        requestLocationPermission()
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val mapView: FragmentContainerView = binding.mapFragment
         val mapFragment = childFragmentManager.findFragmentById(mapView.id) as MapFragment?
         mapFragment?.getMapAsync { mapView ->
+            requestLocationPermission()
             val cameraPosition = CameraPosition(LatLng(37.540693, 127.07023), 10.0)
             mapView.cameraPosition = cameraPosition
 
@@ -85,6 +91,19 @@ class HomeFragment : Fragment() {
             marker2.position = LatLng(37.540693, 127.07023) // 마커의 위치 설정
             marker2.map = mapView
             // 다른 마커를 추가하려면 위의 코드를 반복
+
+            val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                location?.let {
+                    val currentLatLng = LatLng(it.latitude, it.longitude)
+
+                    // 현재 위치를 지도에 표시
+                    val marker = Marker()
+                    marker.icon = OverlayImage.fromResource(R.drawable.current_location_icon)
+                    marker.position = currentLatLng
+                    marker.map = mapView
+                }
+            }
         }
         homeViewModel.text.observe(viewLifecycleOwner) {
             mapView.id
@@ -112,17 +131,39 @@ class HomeFragment : Fragment() {
     }
 
     private fun requestLocationPermission() {
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+        val context: Context = requireContext()
+        val fineLocationPermission = Manifest.permission.ACCESS_FINE_LOCATION
+        val coarseLocationPermission = Manifest.permission.ACCESS_COARSE_LOCATION
+
+        if (ContextCompat.checkSelfPermission(context, fineLocationPermission) == PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(context, coarseLocationPermission) == PackageManager.PERMISSION_GRANTED) {
+            // 권한이 이미 허용되어 있음
+            // 현재 위치를 가져오는 코드를 이곳에 추가할 수 있음
+        } else {
+            // 권한을 요청
             ActivityCompat.requestPermissions(
                 requireActivity(),
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                arrayOf(fineLocationPermission, coarseLocationPermission),
                 LOCATION_PERMISSION_REQUEST_CODE
             )
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 사용자에 의해 허용됨
+                // 현재 위치를 가져오는 코드를 이곳에 추가할 수 있음
+            } else {
+                // 권한이 거부됨
+                Toast.makeText(requireContext(), "위치 권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 
 }
