0ed6b6073b3bd7fa55a11ad50413df69fe4d8c8a
Kwon Mingyu <kmgmg230601@gmail.com>
2023-11-16 14:52:55 +0900

    프로필 기본 ㅡ레임 제작

--- app/src/main/java/com/example/seoulfesmap/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/seoulfesmap/ui/profile/ProfileFragment.kt
@@ -37,10 +37,6 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
 
         return inflater.inflate(R.layout.fragment_profile, container, false)
